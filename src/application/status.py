13e9a4d99b900e0061dc015f9d007cda5f6bce23
Jackson <jack.teruya@gmail.com>
2024-09-17 21:39:59 -0400

    add status

---
+++ src/application/status.py
@@ -0,0 +1,9 @@
+from src.response import ResponseTypes
+
+STATUS_CODES = {
+    ResponseTypes.SUCCESS: 200,
+    ResponseTypes.CREATE: 201,
+    ResponseTypes.RESOURCE_ERROR: 404,
+    ResponseTypes.PARAMETERS_ERROR: 400,
+    ResponseTypes.SYSTEM_ERROR: 500,
+}
