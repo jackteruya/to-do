e1d9d5ecf9cc91a06a0a84f8993f6b792962edc4
Jackson <jack.teruya@gmail.com>
2024-09-18 07:22:48 -0400

    map fields table and init alembic to migration

---
+++ src/infra/database/entities/todo.py
@@ -0,0 +1,30 @@
+from datetime import date, datetime
+
+from sqlalchemy import (
+    String,
+    BOOLEAN,
+    Date,
+    DateTime,
+    func,
+)
+from sqlalchemy.orm import mapped_column, Mapped
+
+from src.infra.database.config import Base
+
+
+class ToDo(Base):
+    """To Do Model"""
+
+    __tablename__ = "todo"
+
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    title: Mapped[str] = mapped_column(String(128))
+    description: Mapped[str] = mapped_column(String(255), nullable=True)
+    completed: Mapped[str] = mapped_column(BOOLEAN, default=False)
+    start_date: Mapped[date] = mapped_column(Date, nullable=True)
+    end_date: Mapped[date] = mapped_column(Date, nullable=True)
+    create_at: Mapped[date] = mapped_column(DateTime, default=func.now())
+    update_at: Mapped[date] = mapped_column(DateTime, default=func.now(), onupdate=func.now())
+
+    def __repr__(self):
+        return f"ToDo: [id={self.id}, title={self.title}]"
