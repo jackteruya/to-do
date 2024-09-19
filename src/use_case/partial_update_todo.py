56a08ce4c7548c9489d65fb096ffbbb4cc1d2af9
Jackson <jack.teruya@gmail.com>
2024-09-19 06:24:12 -0400

    add test to update partial todo and update use case to update partial

--- src/use_case/partial_update_todo.py
+++ src/use_case/partial_update_todo.py
@@ -12,11 +12,20 @@ class PartialUpdateToDoUseCase:
     def execute(self, id: int, data: dict):
         try:
             update_data = self.validate_data(data)
+            if update_data is None:
+                return ResponseFailure(
+                    ResponseTypes.PARAMETERS_ERROR,
+                    'Validation Fields: ['
+                    '{"title": "string", '
+                    '"description": "string", '
+                    '"completed": "boolean", '
+                    '"start_date": "date", '
+                    '"end_date": "date"}]'
+                )
             if self._todo_repository.edit_partial_todo(id, update_data):
                 result = self._todo_repository.get_by_id(id)
-                if not result:
-                    return ResponseFailure(ResponseTypes.NOT_FOUND_ERROR, 'Not Found')
-                return ResponseSuccess(result)
+                if result:
+                    return ResponseSuccess(result)
             return ResponseFailure(ResponseTypes.NOT_FOUND_ERROR, 'Not Found')
         except Exception as ex:
             return ResponseFailure(ResponseTypes.SYSTEM_ERROR, 'System Error')
@@ -24,9 +33,15 @@ class PartialUpdateToDoUseCase:
     @staticmethod
     def validate_data(data: dict):
         update_data = dict()
-        for k, v in data.items():
-            if v is not None:
-                if isinstance(v, str) and re.match(r"(\d{4}-\d{2}-\d{2})", v):
-                    v = datetime.strptime(v, '%Y-%m-%d')
-                update_data[k] = v
-        return update_data
+        try:
+            for k, v in data.items():
+                if v is not None:
+                    if k in ['start_date', 'end_date'] and re.match(r"(\d{4}-\d{2}-\d{2})", v):
+                        v = datetime.strptime(v, '%Y-%m-%d')
+                    if k == 'completed':
+                        if not isinstance(v, bool):
+                            return None
+                    update_data[k] = v
+            return update_data
+        except:
+            return None
