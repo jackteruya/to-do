b16167b6267d3f69ae350fd07e03fd98f389adce
Jackson <jack.teruya@gmail.com>
2024-09-19 06:24:34 -0400

    add test to list todo

---
+++ tests/test_application/test_list_todo.py
@@ -0,0 +1,26 @@
+from unittest import mock
+
+from src.infra.database.repository.todo_repository import ToDoRepository
+
+
+def test_list_todo(create_todo_tester, client):
+    result = client.get(
+        f"api/v1/todo/"
+    )
+    assert result.status_code == 200
+
+
+@mock.patch.object(ToDoRepository, "list_todos", return_value=None)
+def test_list_todo_not_found(mock_list_todos, client):
+    result = client.get(
+        f"api/v1/todo/"
+    )
+    assert result.status_code == 400
+
+
+@mock.patch.object(ToDoRepository, "list_todos", side_effect=Exception())
+def test_list_todo_system_error(mock_list_todos, client):
+    result = client.get(
+        f"api/v1/todo/"
+    )
+    assert result.status_code == 500
